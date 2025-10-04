eb40a00fd8016f032b076dbcc9c4a6e0105809a9
Ondra Copak <ondra.copak@gmail.com>
2025-10-04 18:18:02 +0200

    subcubes, rotation basics

--- scripts/classes/cube.gd
+++ scripts/classes/cube.gd
@@ -2,8 +2,18 @@
 class_name Cube
 extends Node3D
 
-var sides: Dictionary = {}  # Dictionary[Side.SIDE, Side] in comment for clarity
-var subcubs: Array[Subcube] = []
+var sides: Dictionary = {}
+
+# using Dict, instead of list for faster adding/popping - hashtable
+var subcubs: Dictionary = {
+	Side.SIDE.TOP: {},
+	Side.SIDE.BOTTOM: {},
+	Side.SIDE.FRONT: {},
+	Side.SIDE.BACK: {},
+	Side.SIDE.LEFT: {},
+	Side.SIDE.RIGHT: {},
+}
+
 var is_rotating: bool = false
 
 func set_side(side_node: Node3D, side_enum: Side.SIDE) -> void:
@@ -15,18 +25,87 @@ func set_subcube(subcube_node: Node3D, side_enums: Array[Side.SIDE]) -> void:
 	var s: Subcube = subcube_node
 	s.set_side_membership(side_enums)
 	
+# TODO: make this signal, because its called by kids.
+func subcubes_dict_modify(subcube: Subcube, side_enum: Side.SIDE, add: bool) -> void:
+	if add:
+		subcubs[side_enum][subcube] = true;
+	elif subcubs[side_enum][subcube]:
+		subcubs[side_enum].erase(subcube)
 
-
-
+# calculatng middle point of all sides using the average of all subcubes
+func get_current_middle(subcubes_given_side: Array[Subcube]) -> Vector3:
+	var total := Vector3.ZERO
+	var count := 0
+	for subcube: Node3D in subcubes_given_side:
+		total += subcube.global_position
+		count += 1
+	return total / count if count > 0 else Vector3.ZERO
+	
 func rotate_cube(side: Side.SIDE) -> void:
 	if is_rotating:
 		return
+	is_rotating = true
+	
+	rotate_subcubes(side, 90.0, 0.5)
+	is_rotating = false
+	
+func rotate_subcubes(side: Side.SIDE, rot_deg: float, duration: float):
+	var subcubes_given_side: Array[Subcube] = []
+	for key in subcubs[side]:
+		subcubes_given_side.append(key)
+		
+	var middle: Vector3 = get_current_middle(subcubes_given_side)
+	var axis := Side.get_vector_from_side(side)
+
+	# temp pivot to rotate around
+	var pivot := Node3D.new()
+	pivot.position = middle
+	get_parent().add_child(pivot)
+	pivot.global_position = middle
+	
+	# adds all subcubes to the pivot
+	for subcube: Node3D in subcubes_given_side:
+		subcube.reparent(pivot)
+		
+	# rotates the pivot (and all of the subsides)
+	var tween = get_tree().create_tween()
+	tween.tween_property(
+		pivot, 
+		"rotation_degrees", 
+		pivot.rotation_degrees + axis * rot_deg, 
+		duration
+	)
+	
+	await tween.finished # wait for the rotation to end
+
+	# returns the sturcture to the origianl state
+	for subcube: Node3D in subcubes_given_side:
+		subcube.reparent(self)
+
+	pivot.queue_free()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	"""
 		
 	var side_node: Side = sides[side]
 	if not side_node:
 		printerr("side doesnt exist, define the side using Cube.set_side(Node3D, Side.SIDE)")
 		return
 	
-	is_rotating = true
+	
 	await side_node.rotate_side(90.0, 0.5)
-	is_rotating = false
+	is_rotating = false"""
--- scripts/classes/side.gd
+++ scripts/classes/side.gd
@@ -9,20 +9,8 @@ var side: SIDE
 var middle: Vector3 = Vector3.ZERO
 var cube_parent: Cube
 
-func _ready() -> void:
-	middle = get_current_middle() # sets th current middle
-
-# calculatng middle point of all sides using the average of all subcubes
-func get_current_middle() -> Vector3:
-	var total := Vector3.ZERO
-	var count := 0
-	for subcube: Node3D in get_children():
-		total += subcube.global_position
-		count += 1
-	return total / count if count > 0 else Vector3.ZERO
-
 # returns the unit vector based of the SIDE enum
-func get_vector_from_side(s: SIDE) -> Vector3:
+static func get_vector_from_side(s: SIDE) -> Vector3:
 	match s:
 		SIDE.TOP: return Vector3.UP
 		SIDE.BOTTOM: return Vector3.DOWN
@@ -34,36 +22,9 @@ func get_vector_from_side(s: SIDE) -> Vector3:
 
 # rotates around given axe by "rot_deg" for "duration" seconds
 func rotate_side(rot_deg: float, duration: float) -> void:
-	var axis := get_vector_from_side(side)
-	middle = get_current_middle()
+	pass
 
-	# temp pivot to rotate around
-	var pivot := Node3D.new()
-	pivot.position = middle
-	get_parent().add_child(pivot)
-	pivot.global_position = middle
-	
-	# adds all subcubes to the pivot
-	for subcube: Node3D in get_children():
-		subcube.reparent(pivot)
-		
-	# rotates the pivot (and all of the subsides)
-	var tween = get_tree().create_tween()
-	tween.tween_property(
-		pivot, 
-		"rotation_degrees", 
-		pivot.rotation_degrees + axis * rot_deg, 
-		duration
-	)
-	
-	await tween.finished # wait for the rotation to end
-
-	# returns the sturcture to the origianl state
-	for subcube: Node3D in pivot.get_children():
-		subcube.reparent(self)
-
-	pivot.queue_free()
-	
 func set_enum(s: SIDE, c: Cube) -> void:
 	side = s
 	cube_parent = c
+	
