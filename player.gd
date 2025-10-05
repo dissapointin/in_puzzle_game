c5a454045896a3065e39f65eb47e2920780ea5b0
om <ondra.hrabe@seznam.cz>
2025-10-05 11:57:14 +0200

    locked mouse and cube rotation input

--- player.gd
+++ player.gd
@@ -3,6 +3,12 @@ extends CharacterBody3D
 @export var speed : float
 @export var camera_sens : Vector2 = Vector2(0.5, 0.3)
 @onready var camera : Camera3D = $Camera3D
+var mouse_direction : float = 0
+
+signal move_cube_input
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -22,18 +28,29 @@ func _physics_process(delta: float) -> void:
 	pass
 	
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("move_cube"):
+		_rotate_cube()
 	pass
 
 	
 func _input(event: InputEvent):
-	print(1)
 	if event is InputEventMouseMotion:
 		_rotate_camera(event.relative.x, event.relative.y)
+		mouse_direction = sign(event.relative.x)
+		
 
 	
 func _rotate_camera(x, y):
 		rotation.y -= x * camera_sens.y
 		camera.rotation.x -= y * camera_sens.x
 		
-
+func _rotate_cube():
+	var ray: RayCast3D = camera.raycast
+	ray.target_position = Vector3.FORWARD * 20
+	print(ray.is_colliding())
+	var normal: Vector3 = round(ray.get_collision_normal())
+	print(mouse_direction)
+	emit_signal("move_cube_input", normal, mouse_direction)
+	pass
+ 
 	
---
+++ player_camera.gd
@@ -0,0 +1,3 @@
+extends Camera3D
+
+@onready var raycast: RayCast3D = $RayCast3D
